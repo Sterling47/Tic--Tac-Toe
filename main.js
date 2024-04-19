7a0c2323ee5c81caceb9434b128ac9a2af26df18
Adam Konber <konber3@gmail.com>
2024-04-18 20:13:02 -0600

    Created function that stores players name, id, token, and total wins

---
+++ main.js
@@ -0,0 +1,18 @@
+function playerInfo(name, id, token) {
+    var player = {
+        name: name,
+        id: id,
+        token: token,
+        wins: 0
+    }
+    return player
+}
+
+var player1 = playerInfo('player 1', 1, 'X')
+
+var player2 = playerInfo('player 2', 2, 'O')
+
+
+
+
+
