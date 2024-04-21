3361d8b6ce4600480ead266b08e524615e3079e9
Adam Konber <konber3@gmail.com>
2024-04-21 10:08:58 -0600

    Added a function that checks if the cells thats clicked is empty or not.

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
+// Query Selectors
+var mainSection = document.querySelector('.mainboard-section')
+var game_Board = document.querySelector('board-box') 
+var player1Section = document.querySelector('.player-1')
+var player2Section = document.querySelector('.player-2')
+var gameTitle = document.querySelector('.game-title')
+var cells = document.querySelectorAll('.cell')
 
+
+// Global Variables
 var currentPlayer;
 var player1;
 var player2;
+var board;
 
-function playerInfo(name, id, token) {
+//Event Listeners
+
+
+//functions
+function createPlayer(name, id, token) {
     var player = {
         name: name,
         id: id,
@@ -13,29 +27,37 @@ function playerInfo(name, id, token) {
     return player
 }
 
-player1 = playerInfo('player 1', 1, 'X')
-player2 = playerInfo('player 2', 2, 'O')
-
-
-function addWin(winner) {
-    winner.wins++
-    return winner
-}
-
 function gameBoard() {
-    return [
+    board = [
         ['', '', ''],
         ['', '', ''],
         ['', '', '']
     ]
 }
 
-function currentPlayersTurn(){
-    if(currentPlayer === player1){
+function addWin(winner) {
+    winner.wins++
+    player1Section.innerHTML = `
+    <h2 class="p1-header">player 1</h2>
+    <h2 class="p1=score">${winner.wins}</h2>
+    `
+}
+
+function currentPlayersTurn() {
+    if (currentPlayer === player1) {
         currentPlayer = player2
     } else {
         currentPlayer = player1
     }
+    gameTitle.innerText = `${currentPlayer.name} turn!`
 }
 
+function checkIfEmpty(e){
+ if(!e.target.textContent){
+    e.target.textContent = currentPlayer.token
+    currentPlayersTurn();
+ } else {
+    alert('pick a empty cell')
+ }
+}
 
